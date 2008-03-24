b633bc14047c02cac2569baa41cf6242627b8a26
sune.t.jensen <sune.t.jensen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2008-03-24 12:28:25 +0000

    Updated example config

--- 3Party/Ilib_Services_FakeCPSMS/tests/config.examples.php
+++ 3Party/Ilib_Services_FakeCPSMS/tests/config.examples.php
@@ -1,7 +1,7 @@
 <?php
 
 set_include_path(dirname(__FILE__) . '/../src/' . PATH_SEPARATOR . get_include_path());
-define('ILIB_SERVICES_CPSMS_TEST_USERNAME', 'lsolesen');
-define('ILIB_SERVICES_CPSMS_TEST_PASSWORD', 'UDSLNA');
-define('ILIB_SERVICES_CPSMS_TEST_PHONE', '26176860');
+define('ILIB_SERVICES_CPSMS_TEST_USERNAME', 'username');
+define('ILIB_SERVICES_CPSMS_TEST_PASSWORD', 'password');
+define('ILIB_SERVICES_CPSMS_TEST_PHONE', '12345678');
 define('ILIB_SERVICES_CPSMS_TEST_TMPDIR', '/tmp/');
