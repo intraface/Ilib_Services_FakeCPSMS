2825cfe1c3fe2bba4d799f9c90a2035b520b69cb
lsolesen <lsolesen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2010-05-19 21:45:52 +0000

    Coding standard fixes.

--- 3Party/Ilib_Services_FakeCPSMS/src/Ilib/Services/FakeCPSMS.php
+++ 3Party/Ilib_Services_FakeCPSMS/src/Ilib/Services/FakeCPSMS.php
@@ -1,23 +1,23 @@
 <?php
 /**
  * Class for easy testing of CPSMS gateway.
- * 
+ *
  * PHP Version 5
- * 
+ *
  * @category Services
  * @package Ilib_Services_FakeCPSMS
  * @author   Sune Jensen <sj@sunet.dk>
  * @author   Lars Olesen <lars@legestue.net>
- * 
+ *
  */
 
 require_once 'Ilib/Services/CPSMS.php';
 
 /**
  * Class for easy testing of CPSMS gateway.
- * 
+ *
  * Extends Ilib_Services_CPSMS
- * 
+ *
  * Instead of sending sms the it is written to
  * a log file
  *
@@ -34,16 +34,16 @@ require_once 'Ilib/Services/CPSMS.php';
  * @package  Ilib_Services_FakeCPSMS
  * @author   Sune Jensen <sj@sunet.dk>
  * @author   Lars Olesen <lars@legestue.net>
- * 
+ *
  */
 class Ilib_Services_FakeCPSMS extends Ilib_Services_CPSMS
 {
-    
+
     private $tmpdir;
 
     /**
      * Constructor
-     * 
+     *
      * @param string username
      * @param string password
      * @param string sender name
@@ -54,42 +54,39 @@ class Ilib_Services_FakeCPSMS extends Ilib_Services_CPSMS
         parent::__construct($username, $password, $sendername);
         $this->tmpdir = $tmp_directory;
     }
-    
+
     /**
      * Writes sms to log file
-     * 
+     *
      * @return boolean true on success
      */
     public function send()
     {
-        if(empty($this->message)) {
+        if (empty($this->message)) {
             $this->setErrorMessage('The message is empty.');
             return false;
         }
-        
-        if(!is_array($this->recipient) || empty($this->recipient)) {
+
+        if (!is_array($this->recipient) || empty($this->recipient)) {
             $this->setErrorMessage('No recipients is given.');
             return false;
         }
-        
+
         $send = "&message=" . $this->message;
-        
-        if(count($this->recipient) > 1 ) {
-            foreach($this->recipient AS $recipient) {
+
+        if (count($this->recipient) > 1 ) {
+            foreach ($this->recipient AS $recipient) {
                 $send .= "&recipient[]=" . $recipient; // Recipient
             }
-        }
-        else {
+        } else {
             $send .= "&recipient=" . $this->recipient[0]; // Recipient
         }
-        
+
         // The url is opened
-        if(file_put_contents($this->tmpdir.DIRECTORY_SEPARATOR.'Ilib_Services_FakeCPSMS_SMSLog.txt', $this->url.$send.PHP_EOL, FILE_APPEND)) {
+        if (file_put_contents($this->tmpdir.DIRECTORY_SEPARATOR.'Ilib_Services_FakeCPSMS_SMSLog.txt', $this->url.$send.PHP_EOL, FILE_APPEND)) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 }
-?>
